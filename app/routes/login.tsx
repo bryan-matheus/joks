b703a4b21da60bef9b09225a620fb855a2e08c2a
Bryan Matheus <bryan.matheusbmf@gmail.com>
2022-02-11 21:42:09 -0300

    Added SEO meta tags

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -1,4 +1,8 @@
-import type { ActionFunction, LinksFunction } from "remix";
+import type {
+    ActionFunction,
+    LinksFunction,
+    MetaFunction
+} from "remix";
 import {
     useActionData,
     json,
@@ -16,6 +20,14 @@ import stylesUrl from "~/styles/login.css";
 
 export const links: LinksFunction = () => {
     return [{ rel: "stylesheet", href: stylesUrl }];
+};
+
+export const meta: MetaFunction = () => {
+    return {
+        title: "Remix Jokes | Login",
+        description:
+            "Login to submit your own jokes to Remix Jokes!"
+    };
 };
 
 function validateUsername(username: unknown) {
