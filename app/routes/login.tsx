0c4b644494f592275211928e42aa5d329449c1f3
Bryan Matheus <bryan.matheusbmf@gmail.com>
2022-02-11 21:34:34 -0300

    Added register function

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -2,13 +2,17 @@ import type { ActionFunction, LinksFunction } from "remix";
 import {
     useActionData,
     json,
-    Link,
-    useSearchParams
+    useSearchParams,
+    Link
 } from "remix";
 
 import { db } from "~/utils/db.server";
+import {
+    createUserSession,
+    login,
+    register
+} from "~/utils/session.server";
 import stylesUrl from "~/styles/login.css";
-import { createUserSession, login } from "~/utils/session.server";
 
 export const links: LinksFunction = () => {
     return [{ rel: "stylesheet", href: stylesUrl }];
@@ -72,7 +76,6 @@ export const action: ActionFunction = async ({
     switch (loginType) {
         case "login": {
             const user = await login({ username, password });
-            console.log({ user });
             if (!user) {
                 return badRequest({
                     fields,
@@ -91,12 +94,14 @@ export const action: ActionFunction = async ({
                     formError: `User with username ${username} already exists`
                 });
             }
-            // create the user
-            // create their session and redirect to /jokes
-            return badRequest({
-                fields,
-                formError: "Not implemented"
-            });
+            const user = await register({ username, password });
+            if (!user) {
+                return badRequest({
+                    fields,
+                    formError: `Something went wrong trying to create a new user.`
+                });
+            }
+            return createUserSession(user.id, redirectTo);
         }
         default: {
             return badRequest({
