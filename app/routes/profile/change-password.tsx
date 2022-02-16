72a034f3e185653b33a0df8e4d96de31b4c9ce37
Bryan Matheus <bryan.matheusbmf@gmail.com>
2022-02-15 22:02:23 -0300

    Added change password feature

--- app/routes/profile/change-password.tsx
+++ app/routes/profile/change-password.tsx
@@ -1,50 +1,243 @@
-import { User } from "@prisma/client";
-import { Form, LoaderFunction, useLoaderData } from "remix";
-import { getUser } from "~/utils/session.server";
+import {
+    ActionFunction,
+    Form,
+    json,
+    Link,
+    LoaderFunction,
+    redirect,
+    useActionData,
+    useCatch,
+    useTransition
+} from "remix";
+import { db } from "~/utils/db.server";
+import { getUserId, requireUserId } from "~/utils/session.server";
+import bcrypt from "bcryptjs";
 
-type LoaderData = {
-    user: User;
+type ActionData = {
+    formError?: string;
+    fieldErrors?: {
+        currentPassword: string | undefined;
+        newPassword: string | undefined;
+        repeatNewPassword: string | undefined;
+    };
+    fields?: {
+        currentPassword: string;
+        newPassword: string;
+        repeatNewPassword: string;
+    };
 };
 
-export const loader: LoaderFunction = async ({request}) => {
-    const user = await getUser(request);
+export const loader: LoaderFunction = async ({ request }) => {
+    const userId = await getUserId(request);
 
-    const data: LoaderData = {
-        user: user as User,
+    if (!userId) {
+        throw new Response("Unauthorized", { status: 401 });
+    }
+
+    return {};
+};
+
+const badRequest = (data: ActionData) => {
+    return json(data, { status: 400 });
+}
+
+function validateCurrentPassword(password: string) {
+    if (password.length < 8) {
+        return `That password is too short`;
+    }
+}
+
+function validateRepeatPassword(password: string, repeatPassword: string) {
+    if (password !== repeatPassword) {
+        return `Passwords do not match`;
+    }
+}
+function validateNewPassword(password: string) {
+    if (password.length < 8) {
+        return `That password is too short`;
+    }
+
+    if (password.length > 255) {
+        return `That password is too long`;
+    }
+}
+
+export const action: ActionFunction = async ({ request }) => {
+    const userId = await requireUserId(request);
+    const form = await request.formData();
+    const currentPassword = form.get("currentPassword");
+    const repeatNewPassword = form.get("repeatNewPassword");
+    const newPassword = form.get("newPassword");
+
+    const currentUser = await db.user.findUnique({
+        where: {
+            id: userId,
+        },
+    });
+
+    if (
+        typeof currentPassword !== "string" ||
+        typeof repeatNewPassword !== "string" ||
+        typeof newPassword !== "string"
+    ) {
+        return badRequest({
+            formError: `Form not submitted correctly.`
+        });
+    }
+
+    const fieldErrors = {
+        currentPassword: validateCurrentPassword(currentPassword),
+        repeatNewPassword: validateRepeatPassword(newPassword, repeatNewPassword),
+        newPassword: validateNewPassword(newPassword),
     };
 
-    return data;
+    const comparedPassword = await bcrypt.compare(currentPassword, currentUser?.passwordHash as string);
+
+    if (!comparedPassword) {
+        fieldErrors.currentPassword = "Incorrect password";
+    }
+
+    const fields = { currentPassword, repeatNewPassword, newPassword };
+
+    if (Object.values(fieldErrors).some(Boolean)) {
+        return badRequest({ fieldErrors, fields });
+    }
+
+    const passwordHash = await bcrypt.hash(newPassword, 10);
+
+    const user = await db.user.update({
+        data: { passwordHash },
+        where: { id: userId },
+    });
+
+    if (!user) {
+        return badRequest({
+            formError: `Could not update password.`,
+        });
+    }
+
+    return redirect("/profile");
 };
 
 export default function ProfileEditRoute() {
-    const data = useLoaderData<LoaderData>();
+    const actionData = useActionData<ActionData>();
+    const transition = useTransition();
 
     return (
         <div>
             <h4>Change your password</h4>
 
-            <Form>
-                <label>
-                    Current password: {" "}
-                    <input type="password" placeholder="Your current password" />
-                </label>
+            <Form method="patch">
+                <div>
+                    <label>
+                        Current password: {" "}
+                        <input
+                            type="password"
+                            name="currentPassword"
+                            placeholder="Your current password"
+                            value={actionData?.fields?.currentPassword}
+                            defaultValue={actionData?.fields?.currentPassword}
+                            aria-invalid={
+                                Boolean(actionData?.fieldErrors?.currentPassword) ||
+                                undefined
+                            }
+                            aria-describedby={
+                                actionData?.fieldErrors?.currentPassword
+                                    ? "new-password-error"
+                                    : undefined
+                            } />
+                    </label>
+                    {actionData?.fieldErrors?.currentPassword ? (
+                        <p className="form-validation-error"
+                            role="alert"
+                            id="current-password-error">
+                            {actionData.fieldErrors.currentPassword}
+                        </p>
+                    ) : null}
+                </div>
 
-                <label>
-                    New password: {" "}
-                    <input 
-                        type="password" 
-                        placeholder="Add new password" />
-                </label>
+                <div>
+                    <label>
+                        New password: {" "}
+                        <input
+                            type="password"
+                            name="newPassword"
+                            placeholder="Add new password"
+                            value={actionData?.fields?.newPassword}
+                            defaultValue={actionData?.fields?.newPassword}
+                            aria-invalid={
+                                Boolean(actionData?.fieldErrors?.newPassword) ||
+                                undefined
+                            }
+                            aria-describedby={
+                                actionData?.fieldErrors?.newPassword
+                                    ? "new-password-error"
+                                    : undefined
+                            } />
+                    </label>
+                    {actionData?.fieldErrors?.newPassword ? (
+                        <p className="form-validation-error"
+                            role="alert"
+                            id="new-password-error">
+                            {actionData.fieldErrors.newPassword}
+                        </p>
+                    ) : null}
+                </div>
 
-                <label>
-                    Repeat new password: {" "}
-                    <input 
-                        type="password" 
-                        placeholder="Repeat new password" />
-                </label>
+                <div>
+                    <label>
+                        Repeat new password: {" "}
+                        <input type="password"
+                            name="repeatNewPassword"
+                            placeholder="Repeat new password"
+                            value={actionData?.fields?.repeatNewPassword}
+                            defaultValue={actionData?.fields?.repeatNewPassword}
+                            aria-invalid={
+                                Boolean(actionData?.fieldErrors?.repeatNewPassword) ||
+                                undefined
+                            }
+                            aria-describedby={
+                                actionData?.fieldErrors?.repeatNewPassword
+                                    ? "repeat-new-password-error"
+                                    : undefined
+                            } />
+                    </label>
+                    {actionData?.fieldErrors?.repeatNewPassword ? (
+                        <p className="form-validation-error"
+                            role="alert"
+                            id="repeat-new-password-error">
+                            {actionData.fieldErrors.repeatNewPassword}
+                        </p>
+                    ) : null}
+                </div>
 
-                <button type="submit" className="button">Change your password</button>
+                <button type="submit" className="button">
+                    {transition.state === "submitting" ? "Changing your password..." : "Change password"}
+                </button>
             </Form>
+        </div>
+    );
+}
+
+export function CatchBoundary() {
+    const caught = useCatch();
+
+    if (caught.status === 401) {
+        return (
+            <div className="error-container">
+                <p>You must be logged in to edit profile.</p>
+                <Link to="/login">Login</Link>
+            </div>
+        );
+    }
+}
+
+export function ErrorBoundary({ error }: { error: Error }) {
+    console.error(error);
+
+    return (
+        <div className="error-container">
+            Something unexpected went wrong. Sorry about that.
         </div>
     );
 }
